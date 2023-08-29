b667d9000b323f46c31965c70199cee67f052c19
ms.rafiee <srafiee@dpco.net>
2023-08-29 09:29:12 +0330

    rename Creator interface into Factory in Factory Method Creational Design Pattern

--- src/main/java/com/patterns/creational/factorymethod/impl/factory/BlueColorFactory.java
+++ src/main/java/com/patterns/creational/factorymethod/impl/factory/BlueColorFactory.java
@@ -1,13 +1,13 @@
 package com.patterns.creational.factorymethod.impl.factory;
 
-import com.patterns.creational.factorymethod.impl.Creator;
+import com.patterns.creational.factorymethod.impl.Factory;
 import com.patterns.creational.factorymethod.impl.Color;
 import com.patterns.creational.factorymethod.impl.product.BlueColor;
 
 /**
  * Blue Color Factory will produce {@link BlueColor}.
  */
-public class BlueColorFactory implements Creator {
+public class BlueColorFactory implements Factory {
     @Override
     public Color factoryMethod() {
         return new BlueColor();
